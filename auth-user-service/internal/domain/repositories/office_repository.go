19092f0e88e966f3a08696fa1b4a3b30baabbb3c
thienel <lethien.dev@gmail.com>
2025-09-17 13:16:17 +0700

    [auth-user-service] feat(repository-persistence): create and implement office repository

---
+++ auth-user-service/internal/domain/repositories/office_repository.go
@@ -0,0 +1,15 @@
+package repositories
+
+import (
+	"auth-service/internal/domain/entities"
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type OfficeRepository interface {
+	Create(ctx context.Context, office *entities.Office) error
+	FindByID(ctx context.Context, id uuid.UUID) (*entities.Office, error)
+	Update(ctx context.Context, office *entities.Office) error
+	SoftDelete(ctx context.Context, id uuid.UUID) error
+}
