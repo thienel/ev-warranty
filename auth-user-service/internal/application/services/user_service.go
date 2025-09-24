de7453998d5a799cef8e8c72caaadfbcbe623ef6
thienel <lethien.dev@gmail.com>
2025-09-24 14:59:14 +0700

    [auth-user-service] feat(services): create user service and modify services to match with entities

--- auth-user-service/internal/application/services/user_service.go
+++ auth-user-service/internal/application/services/user_service.go
@@ -11,9 +11,30 @@ import (
 	"github.com/google/uuid"
 )
 
+type UserCreateParams struct {
+	Name     string
+	Email    string
+	Role     string
+	Password string
+	IsActive bool
+	OfficeID uuid.UUID
+}
+
+type UserUpdateParams struct {
+	Id       uuid.UUID
+	Name     string
+	Email    string
+	Role     string
+	IsActive bool
+	OfficeID uuid.UUID
+}
+
 type UserService interface {
-	Create(ctx context.Context, name, email, role, password string, isActive bool, officeID uuid.UUID) (*entities.User, error)
+	Create(ctx context.Context, params *UserCreateParams) (*entities.User, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*entities.User, error)
+	GetAll(ctx context.Context) ([]*entities.User, error)
+	Update(ctx context.Context, params *UserUpdateParams) error
+	Delete(ctx context.Context, id uuid.UUID) error
 }
 
 type userService struct {
@@ -25,12 +46,15 @@ func NewUserService(userRepo repositories.UserRepository, officeService OfficeSe
 	return &userService{userRepo, officeService}
 }
 
-func (s *userService) GetByID(ctx context.Context, id uuid.UUID) (*entities.User, error) {
-	return s.userRepo.FindByID(ctx, id)
-}
-
-func (s *userService) Create(ctx context.Context, name, email, role, password string, isActive bool, officeID uuid.UUID) (
+func (s *userService) Create(ctx context.Context, params *UserCreateParams) (
 	*entities.User, error) {
+
+	name := params.Name
+	email := params.Email
+	role := params.Role
+	password := params.Password
+	isActive := params.IsActive
+	officeID := params.OfficeID
 
 	email = strings.TrimSpace(email)
 	if !entities.IsValidEmail(email) {
@@ -58,4 +82,54 @@ func (s *userService) Create(ctx context.Context, name, email, role, password st
 	}
 
 	return user, nil
+}
+
+func (s *userService) GetByID(ctx context.Context, id uuid.UUID) (*entities.User, error) {
+	return s.userRepo.FindByID(ctx, id)
+}
+
+func (s *userService) GetAll(ctx context.Context) ([]*entities.User, error) {
+	users, err := s.userRepo.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (s *userService) Update(ctx context.Context, params *UserUpdateParams) error {
+	user, err := s.userRepo.FindByID(ctx, params.Id)
+	if err != nil {
+		return err
+	}
+
+	name := params.Name
+	email := params.Email
+	role := params.Role
+	isActive := params.IsActive
+	officeID := params.OfficeID
+	if user, _ := s.userRepo.FindByEmail(ctx, email); user != nil {
+		return apperrors.ErrInvalidCredentials("invalid email")
+	}
+	if !entities.IsValidEmail(email) {
+		return apperrors.ErrInvalidCredentials("invalid email")
+	}
+	if !entities.IsValidUserRole(role) {
+		return apperrors.ErrInvalidCredentials("invalid role")
+	}
+	if office, err := s.officeService.GetByID(ctx, officeID); err != nil || office == nil {
+		return err
+	}
+
+	user.Name = name
+	user.Email = email
+	user.Role = role
+	user.IsActive = isActive
+	user.OfficeID = officeID
+
+	return s.userRepo.Update(ctx, user)
+}
+
+func (s *userService) Delete(ctx context.Context, id uuid.UUID) error {
+	return s.userRepo.SoftDelete(ctx, id)
 }
