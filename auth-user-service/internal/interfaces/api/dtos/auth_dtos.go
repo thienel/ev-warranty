a04c64026b29ef15d38b7ab6f7e840a829ea630e
thienel <lethien.dev@gmail.com>
2025-09-24 18:03:44 +0700

    [auth-user-service] feat(dtos): create user dtos

--- auth-user-service/internal/interfaces/api/dtos/auth_dtos.go
+++ auth-user-service/internal/interfaces/api/dtos/auth_dtos.go
@@ -1,11 +1,5 @@
 package dtos
 
-import (
-	"auth-service/internal/domain/entities"
-
-	"github.com/google/uuid"
-)
-
 type LoginRequest struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
@@ -15,23 +9,4 @@ type LoginResponse struct {
 	AccessToken  string  `json:"access_token"`
 	RefreshToken string  `json:"refresh_token"`
 	User         UserDTO `json:"user"`
-}
-
-type RegisterRequest struct {
-	Email    string `json:"email" binding:"required,email,max=100"`
-	Password string `json:"password" binding:"required,min=8,max=128"`
-}
-
-type UserDTO struct {
-	ID       uuid.UUID `json:"id" binding:"required"`
-	Email    string    `json:"email" binding:"required,email,max=100"`
-	IsActive bool      `json:"is_active" binding:"required"`
-}
-
-func GenerateUserDTO(user entities.User) *UserDTO {
-	return &UserDTO{
-		ID:       user.ID,
-		Email:    user.Email,
-		IsActive: user.IsActive,
-	}
 }
