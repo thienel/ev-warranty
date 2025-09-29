2a04d0d77dd457cd9c81609285c5bcb6fc5f329c
thienel <lethien.dev@gmail.com>
2025-09-29 14:01:10 +0700

    [frontend] refactor(Login): split LoginForm into component

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -62,3 +62,41 @@ export const API_ENDPOINTS = {
   USER: '/users/',
   OFFICE: '/offices/',
 }
+
+export const PASSWORD_RULES = [
+  {
+    required: true,
+    message: 'Please input your password!',
+  },
+  {
+    min: 8,
+    message: 'Password must be at least 8 characters long!',
+  },
+  {
+    pattern: /[a-z]/,
+    message: 'Password must contain at least one lowercase letter!',
+  },
+  {
+    pattern: /[A-Z]/,
+    message: 'Password must contain at least one uppercase letter!',
+  },
+  {
+    pattern: /\d/,
+    message: 'Password must contain at least one digit!',
+  },
+  {
+    pattern: /[^A-Za-z0-9]/,
+    message: 'Password must contain at least one special character!',
+  },
+]
+
+export const EMAIL_RULES = [
+  {
+    required: true,
+    message: 'Please input your email!',
+  },
+  {
+    type: 'email',
+    message: 'Please enter a valid email address!',
+  },
+]
