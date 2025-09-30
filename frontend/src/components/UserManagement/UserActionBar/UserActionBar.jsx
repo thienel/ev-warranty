e7ccf4a3ae00e2ad442afa734e5507647d7effc2
thienel <lethien.dev@gmail.com>
2025-09-30 13:56:49 +0700

    [frontend] feat(UserManagement): style for UserActionBar

--- frontend/src/components/UserManagement/UserActionBar/UserActionBar.jsx
+++ frontend/src/components/UserManagement/UserActionBar/UserActionBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Col, Input, Row, Space } from 'antd'
+import { Button, Col, Input, Row } from 'antd'
 import { PlusOutlined, ReloadOutlined, SearchOutlined } from '@ant-design/icons'
 import './UserActionBar.less'
 
@@ -13,18 +13,21 @@ const UserActionBar = ({ searchText, setSearchText, onReset, onOpenModal, loadin
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
           allowClear
-          size="large"
         />
       </Col>
-      <Col lg={6} style={{ textAlign: 'right' }}>
-        <Space wrap>
-          <Button icon={<ReloadOutlined />} onClick={onReset} loading={loading}>
-            Refresh
-          </Button>
-          <Button type="primary" icon={<PlusOutlined />} onClick={onOpenModal}>
-            Add User
-          </Button>
-        </Space>
+      <Col lg={6} className={'action-group'}>
+        <Row>
+          <Col lg={12}>
+            <Button icon={<ReloadOutlined />} onClick={onReset} loading={loading}>
+              Refresh
+            </Button>
+          </Col>
+          <Col lg={12}>
+            <Button type="primary" icon={<PlusOutlined />} onClick={onOpenModal}>
+              Add User
+            </Button>
+          </Col>
+        </Row>
       </Col>
     </Row>
   )
