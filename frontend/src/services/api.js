98e3da4fef25cb73dcebb12fd0383e802c0d5175
thienel <lethien.dev@gmail.com>
2025-09-30 19:12:24 +0700

    [frontend] fix(api): resolve wrong api path and access token in response issues

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -29,8 +29,12 @@ api.interceptors.response.use(
     if (status === 401 && !originalRequest._retry) {
       originalRequest._retry = true
       try {
-        const res = await axios.post(API_ENDPOINTS.AUTH.TOKEN, {}, { withCredentials: true })
-        const newToken = res.data.access_token
+        const res = await axios.post(
+          `${API_BASE_URL}${API_ENDPOINTS.AUTH.TOKEN}`,
+          {},
+          { withCredentials: true }
+        )
+        const newToken = res.data.data.access_token
 
         store.dispatch(setToken(newToken))
 
