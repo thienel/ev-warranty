b9b46a8bc2c8ec538dd04be77a74969a411db915
thienel <lethien.dev@gmail.com>
2025-09-30 15:44:35 +0700

    [frontend] feat(Layout): add on click behavior for sidebar header and menu's items

--- frontend/src/components/Layout/Sidebar/Sidebar.jsx
+++ frontend/src/components/Layout/Sidebar/Sidebar.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import { Layout, Menu } from 'antd'
 import {
-  DashboardOutlined,
   UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  SettingOutlined,
   ThunderboltFilled,
+  BankOutlined,
+  CarOutlined,
+  ContainerOutlined,
+  BarChartOutlined,
 } from '@ant-design/icons'
 import './Sidebar.less'
+import { useNavigate } from 'react-router-dom'
 
 const { Sider } = Layout
 
 const Sidebar = ({ collapsed }) => {
   const menuItems = [
-    { key: '1', icon: <DashboardOutlined />, label: 'Dashboard' },
+    { key: '1', icon: <BarChartOutlined />, label: 'Reports' },
     { key: '2', icon: <UserOutlined />, label: 'Users' },
-    { key: '3', icon: <VideoCameraOutlined />, label: 'Media' },
-    { key: '4', icon: <UploadOutlined />, label: 'Uploads' },
-    { key: '5', icon: <SettingOutlined />, label: 'Settings' },
+    { key: '3', icon: <BankOutlined />, label: 'Offices' },
+    { key: '4', icon: <UserOutlined />, label: 'Customers' },
+    { key: '5', icon: <CarOutlined />, label: 'Vehicles' },
+    { key: '6', icon: <ContainerOutlined />, label: 'Warranty claims' },
   ]
+
+  const navigate = useNavigate()
 
   return (
     <Sider
@@ -30,13 +34,42 @@ const Sidebar = ({ collapsed }) => {
       collapsedWidth={80}
       className="sidebar"
     >
-      <div className="sidebar-header">
+      <div className="sidebar-header" onClick={() => navigate('/')}>
         <ThunderboltFilled />
         <div className={`sidebar-title ${collapsed ? 'collapsed' : 'expanded'}`}>
           EV Warranty System
         </div>
       </div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={menuItems} />
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        items={menuItems}
+        onClick={({ key }) => {
+          switch (key) {
+            case '1':
+              navigate('/reports')
+              break
+            case '2':
+              navigate('/users')
+              break
+            case '3':
+              navigate('/offices')
+              break
+            case '4':
+              navigate('/customers')
+              break
+            case '5':
+              navigate('/vehicles')
+              break
+            case '6':
+              navigate('/claims')
+              break
+            default:
+              break
+          }
+        }}
+      />
     </Sider>
   )
 }
