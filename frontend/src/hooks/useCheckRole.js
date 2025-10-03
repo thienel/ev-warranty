3bce9c77b184236992e1486334256ffa37db9c93
thienel <lethien.dev@gmail.com>
2025-10-03 16:12:00 +0700

    [frontend] refactor(hooks): create useCheckRole hook

---
+++ frontend/src/hooks/useCheckRole.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const useCheckRole = (rolesAllowed = []) => {
+  const { user } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user || !rolesAllowed.includes(user?.role)) {
+      navigate('/unauthorized')
+    }
+  }, [rolesAllowed, navigate, user])
+
+  return user
+}
+
+export default useCheckRole
