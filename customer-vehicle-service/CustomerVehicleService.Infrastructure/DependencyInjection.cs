341a501630a04bb37c1d5ef56e61ffd95840438d
markByOverflow <mark.lev1369@gmail.com>
2025-10-06 15:47:57 +0700

    feat: init DependencyInjection

---
+++ customer-vehicle-service/CustomerVehicleService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using CustomerVehicleService.Application.Interfaces;
+using CustomerVehicleService.Application.Interfaces.Data;
+using CustomerVehicleService.Application.Services;
+using CustomerVehicleService.Infrastructure.Data.Context;
+using CustomerVehicleService.Infrastructure.Data.Repositories;
+using CustomerVehicleService.Infrastructure.Data.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+
+namespace CustomerVehicleService.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<CustomerVehicleDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
+            services.AddScoped<IVehicleModelRepository, VehicleModelRepository>();
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<IVehicleService, VehicleService>();
+            services.AddScoped<IVehicleModelService, VehicleModelService>();
+
+            return services;
+        }
+    }
+}
