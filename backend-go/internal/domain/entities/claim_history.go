2ae27e8708b9482ac8dc254062a9dd78bc880630
thienel <lethien.dev@gmail.com>
2025-10-10 10:41:08 +0700

    [backend-go] refactor(entities): add deleted at for soft delete in Claim History

--- backend-go/internal/domain/entities/claim_history.go
+++ backend-go/internal/domain/entities/claim_history.go
@@ -4,13 +4,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type ClaimHistory struct {
-	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
-	ClaimID   uuid.UUID `gorm:"not null;type:uuid" json:"claim_id"`
-	Claim     Claim     `gorm:"foreignKey:ClaimID;references:ID;constraint:OnDelete:CASCADE" json:"-"`
-	Status    string    `gorm:"not null" json:"status"`
-	ChangedBy uuid.UUID `gorm:"not null;type:uuid" json:"changed_by"`
-	ChangedAt time.Time `gorm:"autoCreateTime"`
+	ID        uuid.UUID       `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
+	ClaimID   uuid.UUID       `gorm:"not null;type:uuid" json:"claim_id"`
+	Claim     Claim           `gorm:"foreignKey:ClaimID;references:ID;constraint:OnDelete:CASCADE" json:"-"`
+	Status    string          `gorm:"not null" json:"status"`
+	ChangedBy uuid.UUID       `gorm:"not null;type:uuid" json:"changed_by"`
+	ChangedAt time.Time       `gorm:"autoCreateTime"`
+	DeletedAt *gorm.DeletedAt `gorm:"index" json:"-"`
 }
