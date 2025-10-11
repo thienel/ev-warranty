235b4636bc76b495f6209b7be20158288e0f3565
markByOverflow <mark.lev1369@gmail.com>
2025-10-11 15:37:01 +0700

    feat(migrations): do migrations and update database

--- customer-vehicle-service/CustomerVehicleService.Infrastructure/Migrations/20251011082038_AddVehicleSoftDelete.Designer.cs
+++ customer-vehicle-service/CustomerVehicleService.Infrastructure/Migrations/20251011082038_AddVehicleSoftDelete.Designer.cs
@@ -4,6 +4,7 @@ using CustomerVehicleService.Infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CustomerVehicleService.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251011082038_AddVehicleSoftDelete")]
+    partial class AddVehicleSoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
