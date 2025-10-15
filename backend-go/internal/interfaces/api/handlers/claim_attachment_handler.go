a764bdad5891d477ff1c5622f6c3edef34a30858
thienel <lethien.dev@gmail.com>
2025-10-15 10:21:11 +0700

    feat(handlers): implement all endpoints in claim attachment handler

--- backend-go/internal/interfaces/api/handlers/claim_attachment_handler.go
+++ backend-go/internal/interfaces/api/handlers/claim_attachment_handler.go
@@ -1,11 +1,17 @@
 package handlers
 
 import (
+	"context"
+	"ev-warranty-go/internal/apperrors"
 	"ev-warranty-go/internal/application"
 	"ev-warranty-go/internal/application/services"
+	"ev-warranty-go/internal/domain/entities"
+	"ev-warranty-go/internal/interfaces/api/dtos"
 	"ev-warranty-go/pkg/logger"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type ClaimAttachmentHandler interface {
@@ -29,22 +35,112 @@ func NewClaimAttachmentHandler(log logger.Logger, txManager application.TxManage
 	}
 }
 
-func (c2 claimAttachmentHandler) GetByID(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+func (h *claimAttachmentHandler) GetByID(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), requestTimeout)
+	defer cancel()
+
+	attachmentID, err := parseAttachmentIDParam(c)
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	attachment, err := h.service.GetByID(ctx, attachmentID)
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	writeSuccessResponse(c, http.StatusOK, attachment)
 }
 
-func (c2 claimAttachmentHandler) GetByClaimID(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+func (h *claimAttachmentHandler) GetByClaimID(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), requestTimeout)
+	defer cancel()
+
+	claimID, err := parseClaimIDParam(c)
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	attachments, err := h.service.GetByClaimID(ctx, claimID)
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	writeSuccessResponse(c, http.StatusOK, attachments)
 }
 
-func (c2 claimAttachmentHandler) Create(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+func (h *claimAttachmentHandler) Create(c *gin.Context) {
+	claimID, err := parseClaimIDParam(c)
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	var req dtos.CreateClaimAttachmentRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		handleError(h.log, c, apperrors.NewInvalidJsonRequest())
+		return
+	}
+
+	if !entities.IsValidAttachmentType(req.Type) {
+		handleError(h.log, c, apperrors.NewInvalidCredentials())
+		return
+	}
+
+	cmd := &services.CreateAttachmentCommand{
+		Type: req.Type,
+		URL:  req.URL,
+	}
+
+	var attachment *entities.ClaimAttachment
+	err = h.txManager.Do(c.Request.Context(), func(tx application.Tx) error {
+		var txErr error
+		attachment, txErr = h.service.Create(tx, claimID, cmd)
+		return txErr
+	})
+
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	writeSuccessResponse(c, http.StatusCreated, attachment)
 }
 
-func (c2 claimAttachmentHandler) Delete(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+func (h *claimAttachmentHandler) Delete(c *gin.Context) {
+	claimID, err := parseClaimIDParam(c)
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	attachmentID, err := parseAttachmentIDParam(c)
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	err = h.txManager.Do(c.Request.Context(), func(tx application.Tx) error {
+		return h.service.HardDelete(tx, claimID, attachmentID)
+	})
+
+	if err != nil {
+		handleError(h.log, c, err)
+		return
+	}
+
+	writeSuccessResponse(c, http.StatusOK, gin.H{"message": "Claim attachment deleted successfully"})
+}
+
+func parseAttachmentIDParam(c *gin.Context) (uuid.UUID, error) {
+	attachmentIDStr := c.Param("attachmentID")
+	attachmentID, err := uuid.Parse(attachmentIDStr)
+	if err != nil {
+		return uuid.Nil, apperrors.NewInvalidCredentials()
+	}
+	return attachmentID, nil
 }
