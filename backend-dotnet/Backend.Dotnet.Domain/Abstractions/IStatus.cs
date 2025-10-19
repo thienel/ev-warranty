44f06d1d892aa074e4ddff20cf482a8b3c366e03
markByOverflow <mark.lev1369@gmail.com>
2025-10-20 06:29:11 +0700

    [backend-dotnet] feat(entities): implement status interface for applying Lifecycle State Management

---
+++ backend-dotnet/Backend.Dotnet.Domain/Abstractions/IStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Dotnet.Domain.Abstractions
+{
+    public interface IStatus<TEnum> where TEnum : struct
+    {
+        TEnum Status { get; }
+        void ChangeStatus(TEnum newStatus);
+    }
+}
