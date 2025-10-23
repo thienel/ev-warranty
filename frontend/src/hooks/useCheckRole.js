a1fceecf49413c748df00d58170d504601af5de7
thienel <lethien.dev@gmail.com>
2025-10-23 21:34:15 +0700

    [frontend] feat(hooks): navigate to login page when use is null in useCheckRole

--- frontend/src/hooks/useCheckRole.js
+++ frontend/src/hooks/useCheckRole.js
@@ -1,9 +1,15 @@
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const useCheckRole = (role) => {
   const { user } = useSelector((state) => state.auth)
 
-  return user && role === user.role
+  const navigate = useNavigate()
+  if (!user) {
+    navigate('/login')
+  }
+
+  return role === user.role
 }
 
 export default useCheckRole
