69babfec5d0a7c46f86aec8d4265ccb55744fedb
markByOverflow <mark.lev1369@gmail.com>
2025-10-28 07:22:54 +0700

    [backend-dotnet] feat(): add const for clean code

---
+++ backend-dotnet/Backend.Dotnet.Application/Constants/SystemRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Dotnet.Application.Constants
+{
+    public static class SystemRoles
+    {
+        public const string UserRoleAdmin = "ADMIN";
+        public const string UserRoleEvmStaff = "EVM_STAFF";
+        public const string UserRoleScStaff = "SC_STAFF";
+        public const string UserRoleScTechnician = "SC_TECHNICIAN";
+    }
+}
