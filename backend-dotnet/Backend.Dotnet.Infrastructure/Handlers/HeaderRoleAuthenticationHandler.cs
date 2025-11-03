c44a44772f8975fd4477a3a1b151652edb79954a
markByOverflow <mark.lev1369@gmail.com>
2025-11-03 15:00:01 +0700

    [backend-dotnet] feat(handlers): add header auth handler for user role

---
+++ backend-dotnet/Backend.Dotnet.Infrastructure/Handlers/HeaderRoleAuthenticationHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Backend.Dotnet.Infrastructure.Handler
+{
+    public class HeaderRoleAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public const string SchemeName = "HeaderRoleScheme";
+
+        public HeaderRoleAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock) : base(options, logger, encoder, clock) { }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.TryGetValue("X-User-Role", out var roleHeader))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Missing X-User-Role header"));
+            }
+
+            var role = roleHeader.ToString();
+
+            if (string.IsNullOrEmpty(role))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid role value"));
+            }
+
+            // Learn ClaimsIdentity n ClaimsPrincipal
+            var identity = new ClaimsIdentity(SchemeName);
+            identity.AddClaim(new Claim(ClaimTypes.Role, role));
+            identity.AddClaim(new Claim(ClaimTypes.Name, "HeaderUser"));
+
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, SchemeName);
+
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    }
+}
