fc9de84d1ec3731b6157e7072e650595b6f40d96
markByOverflow <mark.lev1369@gmail.com>
2025-11-05 14:51:02 +0700

    [backend-dotnet] feat(dto): use intermediate model for external to internal trasformation

---
+++ backend-dotnet/Backend.Dotnet.Application/DTOs/IntermediaryDto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Backend.Dotnet.Application.DTOs
+{
+    public class IntermediaryDto
+    {
+        public class ClaimInfo
+        {
+            [JsonPropertyName("id")]
+            public Guid Id { get; set; }
+
+            [JsonPropertyName("vehicle_id")]
+            public Guid VehicleId { get; set; }
+
+            [JsonPropertyName("status")]
+            public string Status { get; set; }
+
+            [JsonPropertyName("description")]
+            public string Description { get; set; }
+        }
+
+        public class TechnicianInfo
+        {
+            [JsonPropertyName("ID")]
+            public Guid Id { get; set; }
+
+            [JsonPropertyName("Name")]
+            public Guid Name { get; set; }
+
+            [JsonPropertyName("Role")]
+            public string Role { get; set; }
+        }
+        public class ClaimItem
+        {
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+
+            [JsonPropertyName("issue_description")]
+            public string IssueDescription { get; set; }
+
+            [JsonPropertyName("part_category_id")]
+            public string PartCategoryId { get; set; }
+
+            [JsonPropertyName("faulty_part_id")]
+            public string FaultyPartId { get; set; }
+
+            [JsonPropertyName("replacement_part_id")]
+            public string ReplacementPartId { get; set; }
+
+            [JsonPropertyName("status")]
+            public string Status { get; set; }
+        }
+    }
+}
