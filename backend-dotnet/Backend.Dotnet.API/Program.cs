54e4d75c6363569c749e4f0e743d5528c81f4f59
markByOverflow <mark.lev1369@gmail.com>
2025-11-11 16:46:36 +0700

    [backend-dotnet] feat: add Authorization Bearer for swagger

--- backend-dotnet/Backend.Dotnet.API/Program.cs
+++ backend-dotnet/Backend.Dotnet.API/Program.cs
@@ -40,6 +40,32 @@ namespace Backend.Dotnet.API
                     }
                 });
 
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Description = "Enter 'Bearer <token>'",
+                    In = ParameterLocation.Header,
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+
                 c.AddSecurityDefinition("X-User-Role", new OpenApiSecurityScheme
                 {
                     In = ParameterLocation.Header,
